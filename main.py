e6fb7dfc82838fe3584e49de3f4b191ec3473e2e
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-10-16 20:06:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
 
+def displaySubfolderTree(absobuteFilePath):
+    print("Subfolder tree of " + absobuteFilePath + ":");
+    for root, dirs, files in os.walk(absobuteFilePath):
+        level = root.replace(absobuteFilePath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}'.format(indent, os.path.basename(root)));
+        subindent = ' ' * 4 * (level + 1);
+        for f in files:
+            print('{}{}'.format(subindent, f));
+
 if __name__ == "__main__":
     #xóa màn hình
     os.system("cls");
-    
-    #in ra màn hình
-    print("Hello World!");
+    absobuteFilePath = input("Enter absolute file path: ");
+    displaySubfolderTree(absobuteFilePath);
