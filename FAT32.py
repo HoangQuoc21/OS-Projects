27c7114483b2790dc99572f506680c24feb8f4a9
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-10-22 15:49:36 +0700

    Thêm 2 module Fat 32 và NTFS

---
+++ FAT32.py
@@ -0,0 +1,9 @@
+# Module này sử dụng cho các hàm liên quan đến các thao tác trên FAT32
+
+#def read_txt_file(file_path):
+    
+
+#def read_other_file(file_path):
+    
+
+#def show_folder_tree(file_path):   
---
+++ NTFS.py
@@ -0,0 +1,9 @@
+# Module này sử dụng cho các hàm liên quan đến các thao tác trên NTFS
+
+#def read_txt_file(file_path):
+    
+
+#def read_other_file(file_path):
+    
+
+#def show_folder_tree(file_path):  
