a8bb5a2b4f399679cd1076d931d157f79352c20d
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-11-03 16:42:17 +0700

    Bổ sung một số comment

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+# Import các thư viện cần thiết:
+
+# Các hàm trong GetVolType.py được sử dụng để xác định loại ổ đĩa
+from GetVolType import *
+
+# Các hàm trong FAT32.py được sử dụng để xử lý ổ đĩa FAT32
 from FAT32.general import *
 from FAT32.directoriesFAT32 import main_FAT32
-from GetVolType import *
+
+# Các hàm trong NTFS.py được sử dụng để xử lý ổ đĩa NTFS
 from NTFS.NTFS import *
 
+# Hàm in data đầu vào theo dạng hexa (thập lục phân)
+def print_hexa(data):
+    print(' '.join('{:02X}'.format(b) for b in data))
+
+#Luồng chính của chương trình
 try:
     platform_name = init_platform()
     volume = input('Type your disk path: ')
@@ -14,9 +26,6 @@ try:
     # Tạo một tệp để mở ổ đĩa và đọc nội dung
     disk_file = generate_disk_file(disk_path)
 
-    def print_hexa(data):
-        print(' '.join('{:02X}'.format(b) for b in data))
-
     if rs == 'FAT32':
         main_FAT32(volume, disk_file)
     elif rs == 'NTFS':
@@ -25,5 +34,6 @@ try:
         print_hexa(data)
         #khi có các hàm xử lý cho NTFS thì xóa 2 dòng trên đi và ghi lại các hàm xử lý NTFS vào
         
-except FileNotFoundError:
-    print("Unknown volume type or Unfound volume!")
+#Xủ lý các lỗi có thể xảy ra        
+except Exception as e:
+    print(f"Error: {e.args}")
