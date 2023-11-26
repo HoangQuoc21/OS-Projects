474a2d5404fe5018ea54985bb9631f366d9cb2b2
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-11-26 15:43:12 +0700

    Review readString.c và bubbleSort.c
    - 2 file chạy tốt
    - Q có sửa lại 2 file một chút để nó dễ nhìn hơn

--- Project02/nachos/nachos-3.4/code/test/bubbleSort.c
+++ Project02/nachos/nachos-3.4/code/test/bubbleSort.c
@@ -2,19 +2,21 @@
 
 int main() {
 	int n, a[101], isAsc, temp, i, j;
-	do {
+	do {	
+		PrintString("n: ");
 		n = ReadInt();
 	} while (n <= 0 || n > 100);
 	for (i = 0; i < n; i++)
 	{
 		a[i] = ReadInt();
 	}
-
+	
+	PrintString("Order (1: Ascending, 0: Descending): ");
 	isAsc = ReadInt();
 
 	for (i = 0 ; i< n;i++) {
 		for (j = 0; j < n - 1; j++) {			
-			if ((isAsc && a[j] < a[j + 1]) || (!isAsc && a[j] > a[j + 1])) {
+			if ((!isAsc && a[j] < a[j + 1]) || (isAsc && a[j] > a[j + 1])) {
 				temp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = temp;
@@ -25,7 +27,8 @@ int main() {
 	for (i = 0; i < n; i++)
 	{
 		PrintInt(a[i]);
+		PrintChar(' ');
 	}
-
-	return 0;
+	PrintChar('\n');
+	Halt();
 }
--- Project02/nachos/nachos-3.4/code/test/readString.c
+++ Project02/nachos/nachos-3.4/code/test/readString.c
@@ -3,7 +3,9 @@
 int main(){
 	char string[50];
 	int length = 50;
+	PrintString("Input your string: ");
 	ReadString(string, length);
 	PrintString(string);
-	return 0;
+	PrintChar('\n');
+	Halt();
 }
