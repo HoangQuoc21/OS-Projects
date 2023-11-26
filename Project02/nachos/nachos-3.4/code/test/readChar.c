23e23a2f0026cfc175c547d22ccab94e359547a7
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-11-26 15:50:53 +0700

    Xóa những file kiểm thử ko còn cần thiết

--- Project02/nachos/nachos-3.4/code/test/readChar.c
+++
@@ -1,7 +0,0 @@
-#include "syscall.h"
-int main() {
-	char c = ReadChar();
-	PrintChar(c);
-	Halt();
-	return 0;
-}
--- Project02/nachos/nachos-3.4/code/test/readInt.c
+++
@@ -1,8 +0,0 @@
-#include "syscall.h"
-int main() {
-	int num;
-	num = ReadInt();
-	PrintInt(num);
-	Halt();
-	return 0;
-}
