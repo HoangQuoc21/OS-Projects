a2f652444324d56c4dbcdac1fbbdcdbcb6af1058
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-11-26 17:34:55 +0700

    Viết và test chương trình help.c

---
+++ Project02/nachos/nachos-3.4/code/test/help.c
@@ -0,0 +1,24 @@
+#include "syscall.h"
+
+int main()
+{
+    	PrintString("====================================================================================\n");
+    	PrintString("*                          HCMUS - OS - Project2                                   *\n");
+	PrintString("====================================================================================\n");
+    	PrintString("* I. Group information:                                                            *\n");
+    	PrintString("*    1. 21120501 - Nguyen Ngoc Gia Minh                                            *\n");
+	PrintString("*    2. 21120542 - Lam Hoang Quoc                                                  *\n");
+	PrintString("*    3. 21120570 - Dang Nguyen Thanh Tin                                           *\n");
+	PrintString("*    4. 21120580 - Tran Thi Kim Trinh                                              *\n");
+	PrintString("*    5. 21120589 - Truong Anh Tuan                                                 *\n");
+    	PrintString("* II. About other programs:                                                        *\n");
+    	PrintString("*    1. bubbleSort.c program: 						        *\n");
+	PrintString("*      - Input n (n <= 100) integer numbers and store them in an array.            *\n");
+	PrintString("*      - Sort the array in ascending or descending order.                          *\n");
+	PrintString("*      - Using algorithm: bubble sort.                 			        *\n");
+	PrintString("*      - Print the sorted array.					                *\n");
+    	PrintString("*    2. ascii.c program:							        *\n");
+	PrintString("*      - Print all printable ascii characters and its corresponding integer value. *\n");
+    	PrintString("====================================================================================\n");
+	Halt();
+}
